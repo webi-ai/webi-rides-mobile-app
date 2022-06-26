9ff8b91d82c11c25dfc2fde06f5b5acf8d4e8ce0
webcoderz <19884161+webcoderz@users.noreply.github.com>
2022-06-25 23:13:49 -0400

    refining

--- src/index.js
+++ src/index.js
@@ -29,15 +29,25 @@ import "@connect2ic/core/style.css"
 //   // EarthWallet
 //   // or create your own (check the existing ones to see how they're implemented)
 // ]
+const renderReactDom = () => {
+    ReactDOM.render(
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  };
 
-ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    renderReactDom();
+  }, false);
+} else {
+  renderReactDom();
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+//serviceWorker.unregister();
